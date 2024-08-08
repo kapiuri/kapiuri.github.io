52e023a6d75e2a5b4d7a7bac5497860e60c3cd9d
kapiuri <116096380+kapiuri@users.noreply.github.com>
2024-08-08 15:46:31 +0200

    ad

--- paginas/hacking/contraseñas/js/script.js
+++ paginas/hacking/contraseñas/js/script.js
@@ -3,12 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const generatedPassword = document.getElementById('generatedPassword');
 
     generatePassword.addEventListener('click', function() {
+        // Definir el conjunto de caracteres permitidos para la contraseña
         const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
         let password = "";
+        
+        // Generar una contraseña de 12 caracteres
         for (let i = 0; i < 12; i++) {
             const randomIndex = Math.floor(Math.random() * charset.length);
             password += charset[randomIndex];
         }
+        
+        // Mostrar la contraseña generada en el div
         generatedPassword.textContent = `Contraseña generada: ${password}`;
     });
 });
