83bbaa683bf4c39a13e732c8f68f1a6fdca61358
kapiuri <116096380+kapiuri@users.noreply.github.com>
2024-08-08 15:42:51 +0200

    h

--- paginas/hacking/contraseñas/js/script.js
+++ paginas/hacking/contraseñas/js/script.js
@@ -1,12 +1,14 @@
-const generatePassword = document.getElementById('generatePassword');
-const generatedPassword = document.getElementById('generatedPassword');
+document.addEventListener('DOMContentLoaded', function() {
+    const generatePassword = document.getElementById('generatePassword');
+    const generatedPassword = document.getElementById('generatedPassword');
 
-generatePassword.addEventListener('click', function() {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-    let password = "";
-    for (let i = 0; i < 12; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset[randomIndex];
-    }
-    generatedPassword.textContent = `Contraseña generada: ${password}`;
+    generatePassword.addEventListener('click', function() {
+        const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+        let password = "";
+        for (let i = 0; i < 12; i++) {
+            const randomIndex = Math.floor(Math.random() * charset.length);
+            password += charset[randomIndex];
+        }
+        generatedPassword.textContent = `Contraseña generada: ${password}`;
+    });
 });
