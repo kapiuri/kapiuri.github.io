8dd4533ce4154216d0c7d4ed32b890e4d8363bb2
kapiuri <116096380+kapiuri@users.noreply.github.com>
2024-08-08 18:09:52 +0200

    Add files via upload

---
+++ paginas/hacking/contraseñas/js/scriptcontraseña.js
@@ -0,0 +1,13 @@
+document.getElementById('generate').addEventListener('click', function() {
+    document.getElementById('password').value = generatePassword(16);
+});
+
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%^&*()_+[]{}|;:',.<>?";
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
