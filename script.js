109d4ef53ed8278f2b3232b29732031f15fafb63
kapiuri <116096380+kapiuri@users.noreply.github.com>
2024-08-09 13:48:08 +0200

    mod

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Dashboard Hacker cargado.');
 });
+
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
