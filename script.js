f66c99aa0f538773a73a7c144a62bfa326788216
kapiuri <116096380+kapiuri@users.noreply.github.com>
2025-03-02 12:52:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Script para manejar el menú móvil
-document.getElementById('menu-movil').addEventListener('click', function() {
-    var enlacesNavegacion = document.getElementById('enlaces-navegacion');
-    enlacesNavegacion.classList.toggle('active');
-    this.classList.toggle('active');
-});
-
-// Script para manejar el año en el footer
-document.getElementById('current-year').textContent = new Date().getFullYear();
