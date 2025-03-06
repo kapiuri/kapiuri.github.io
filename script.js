208623ac8712d87461a84068644910c4a85641a3
kapiuri <116096380+kapiuri@users.noreply.github.com>
2025-03-06 16:50:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,27 +6,30 @@ document.addEventListener("DOMContentLoaded", function() {
     const botonModo = document.getElementById("modo-toggle");
     const body = document.body;
     const icono = botonModo.querySelector("i");
-    const tituloKomodore = document.getElementById("titulo-komodore");
     const footerText = document.getElementById("footer-text");
-    
+    const enlacesFooter = document.querySelectorAll(".informacion a");
+
     // Comprobar si hay una preferencia guardada
     if (localStorage.getItem("modoOscuro") === "true") {
         body.classList.add("modo-oscuro");
         icono.classList.remove("fa-moon");
         icono.classList.add("fa-sun");
+        enlacesFooter.forEach(enlace => enlace.style.color = "#222");
     }
     
     botonModo.addEventListener("click", function() {
         body.classList.toggle("modo-oscuro");
-        
+
         if (body.classList.contains("modo-oscuro")) {
             icono.classList.remove("fa-moon");
             icono.classList.add("fa-sun");
             localStorage.setItem("modoOscuro", "true");
+            enlacesFooter.forEach(enlace => enlace.style.color = "#222");
         } else {
             icono.classList.remove("fa-sun");
             icono.classList.add("fa-moon");
             localStorage.setItem("modoOscuro", "false");
+            enlacesFooter.forEach(enlace => enlace.style.color = "#00ff00");
         }
     });
 });
